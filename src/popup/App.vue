089263ba568bcc9bbb39830cca30e3a6f5972386
bao <243790881@qq.com>
2020-06-11 17:01:20 +0800

    first

---
+++ src/popup/App.vue
@@ -0,0 +1,73 @@
+<template>
+    <div>
+        <div v-if="loading">
+            <p>Loading...</p>
+        </div>
+        <div v-else>
+            CHROM-DEMO-TEST
+            <p class="joke">{{ joke }}</p>
+            <button @click="likeJoke" :disabled="likeButtonDisabled">Like Joke</button>
+            <button @click="logJokes" class="btn">Log Jokes</button>
+            <button @click="clearStorage" class="btn">Clear Storage</button>
+        </div>
+    </div>
+</template>
+<script>
+    import axios from 'axios';
+
+    export default {
+        data() {
+            return {
+                loading: true,
+                joke: "",
+                likeButtonDisabled: false
+            }
+        },
+        methods: {
+            likeJoke() {
+                chrome.storage.local.get("jokes", (res) => {
+                    if (!res.jokes) res.jokes = [];
+                    res.jokes.push(this.joke);
+                    chrome.storage.local.set(res);
+                    this.likeButtonDisabled = true;
+                });
+            },
+            logJokes() {
+                chrome.storage.local.get("jokes", (res) => {
+                    if (res.jokes) res.jokes.map(joke => console.log(joke))
+                });
+            },
+            clearStorage() {
+                chrome.storage.local.clear();
+            }
+        },
+        mounted() {
+            axios.get(
+                "https://icanhazdadjoke.com/",
+                {'headers': {'Accept': 'application/json'}}
+            )
+                .then(res => {
+                    this.joke = res.data.joke;
+                    this.loading = false;
+                });
+        }
+    }
+</script>
+<style>
+    body {
+        height: 98vh;
+        text-align: center;
+        color: #353638;
+        font-size: 22px;
+        line-height: 30px;
+        font-family: Merriweather, Georgia, serif;
+        background-size: 200px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .joke {
+        max-width: 800px;
+    }
+</style>
---
+++ src/popup/popup.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.component("element_ui", ElementUI);
+Vue.use(ElementUI);
+
+
+/* eslint-disable no-new */
+new Vue({
+    el: '#app',
+
+
+    render: h => h(App)
+});
